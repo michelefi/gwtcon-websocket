17e91ff7861bfb4703a43acdc4f1bfd4ca9e4b33
Michele Ficarra <miche.ficarra@gmail.com>
2014-10-05 19:07:07 +0200

    fixed web socket url

--- src/main/java/com/google/developers/gdgfirenze/client/GwtConEntryPoint.java
+++ src/main/java/com/google/developers/gdgfirenze/client/GwtConEntryPoint.java
@@ -3,9 +3,7 @@ package com.google.developers.gdgfirenze.client;
 import com.google.developers.gdgfirenze.client.exception.WebSocketNotSupportedException;
 import com.google.developers.gdgfirenze.client.ui.ChatWidget;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -17,8 +15,7 @@ public class GwtConEntryPoint implements EntryPoint {
 	 */
 	public void onModuleLoad() {
 		
-		// ws://127.0.0.1:8025/chat
-		String url = GWT.getHostPageBaseURL().replace("http", "ws").replaceFirst(":\\d+",":8025") + "chat";
+		String url = "ws://" + Window.Location.getHostName() + ":8025/chat";
 		WebSocket webSocket = new WebSocket(url);
 		ChatWidget view = new ChatWidget();
 		ChatPresenter presenter = new ChatPresenter(view,webSocket);
